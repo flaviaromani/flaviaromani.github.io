24a44c36d277a7c6463faf3e39fbdb764676b672
nikoricci <Nico@Nicolas-MacBook-Air.local>
2016-03-31 23:42:03 -0400

    serving app folder now

---
+++ scripts/controllers/photography.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frbApp.controller:PhotographyCtrl
+ * @description
+ * # PhotographyCtrl
+ * Controller of the frbApp
+ */
+angular.module('frbApp')
+  .controller('PhotographyCtrl', function ($scope) {
+
+	// $( document ).ready(function() {
+	//   	var pattern_container_length = $('#pattern-container').width();
+	//   	var width_of_element = 60;
+	//   	var numOfElements = pattern_container_length / width_of_element;
+	//   	console.log(Math.floor(numOfElements))
+	//   	for (var i=0; i<numOfElements-2; i++) {
+	//   		$('#pattern-container').append('<span><img src="../images/svg2.svg" id="marker1" width="60px"/></span>');
+	//   	}
+	// });
+
+	// $( window ).resize(function() {
+	// 	$('#pattern-container span').remove();
+	//   	var pattern_container_length = $('#pattern-container').width();
+	//   	var width_of_element = 60;
+	//   	var numOfElements = pattern_container_length / width_of_element;
+	//   	console.log(Math.floor(numOfElements))
+	//   	for (var i=0; i<numOfElements-2; i++) {
+	//   		$('#pattern-container').append('<span><img src="../images/svg2.svg" id="marker1" width="60px"/></span>');
+	//   	}
+	// });
+
+  });
